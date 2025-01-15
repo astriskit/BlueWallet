38e5e99b35689623aa9a0b2b8990786fe64e702b
astriskit <harry2office+i2@gmail.com>
2025-01-15 18:10:25 +0530

    added back theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-/** TODO: add theme context */
 import React from 'react';
-// import { useColorScheme } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LargeScreenProvider } from './components/Context/LargeScreenProvider';
 import { SettingsProvider } from './components/Context/SettingsProvider';
-// import { BlueDarkTheme, BlueDefaultTheme } from './components/themes';
+import { BlueDarkTheme, BlueDefaultTheme } from './components/themes';
 import MasterView from './navigation/MasterView';
 import { StorageProvider } from './components/Context/StorageProvider';
+import { ThemeProvider } from '@react-navigation/native';
 
 const App: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
   return (
     <LargeScreenProvider>
-      <SafeAreaProvider>
-        <StorageProvider>
-          <SettingsProvider>
-            <MasterView>{children}</MasterView>
-          </SettingsProvider>
-        </StorageProvider>
-      </SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? BlueDarkTheme : BlueDefaultTheme}>
+        <SafeAreaProvider>
+          <StorageProvider>
+            <SettingsProvider>
+              <MasterView>{children}</MasterView>
+            </SettingsProvider>
+          </StorageProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
     </LargeScreenProvider>
   );
 };
