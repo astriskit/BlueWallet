1a86e5488292d1f3655a08b20bc351dbc06845fd
astriskit <harry2office+i2@gmail.com>
2025-02-13 19:47:15 +0530

    updated test due to use of expo-router

--- tests/unit/deeplink-schema-match.test.js
+++ tests/unit/deeplink-schema-match.test.js
@@ -350,9 +350,9 @@ describe.each(['', '//'])('unit - DeepLinkSchemaMatch', function (suffix) {
     const rez = await asyncNavigationRouteFor({
       url: `bitcoin:${suffix}1DamianM2k8WfNEeJmyqSe2YW1upB7UATx?amount=0.000001&lightning=lnbc1u1pwry044pp53xlmkghmzjzm3cljl6729cwwqz5hhnhevwfajpkln850n7clft4sdqlgfy4qv33ypmj7sj0f32rzvfqw3jhxaqcqzysxq97zvuq5zy8ge6q70prnvgwtade0g2k5h2r76ws7j2926xdjj2pjaq6q3r4awsxtm6k5prqcul73p3atveljkn6wxdkrcy69t6k5edhtc6q7lgpe4m5k4`,
     });
-    assert.strictEqual(rez[0], 'SelectWallet');
-    assert.ok(rez[1].onWalletSelect);
-    assert.ok(typeof rez[1].onWalletSelect === 'function');
+    assert.match(rez.pathname, /SelectWallet$/);
+    assert.ok(rez.params.onWalletSelect);
+    assert.ok(typeof rez.params.onWalletSelect === 'function');
   });
 
   it('decodes bip21', () => {
